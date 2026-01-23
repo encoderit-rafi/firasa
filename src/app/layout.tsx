a2290080cc5d3bc031ac5e7a0e126de3b5ad447c
encoderit-rafi <rafi@encoderit.net>
2026-01-23 17:22:17 +0600

    favicon logo

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+export const metadata: Metadata = {
+  title: "FIRASA",
+  description: "AI Face Personality Score.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable}`}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Logo from "@/assets/Logo";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Logo />
+    </div>
+  );
+}
