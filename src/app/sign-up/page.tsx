686937ab6ea71ce8c0202142576b3b1762d1df80
encoderit-rafi <rafi@encoderit.net>
2026-02-01 16:31:35 +0600

    Update page.tsx

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,3 +1,105 @@
+import { Apple, FacebookFilled, Google } from "@/assets/icons";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { EyeOffIcon } from "lucide-react";
+
 export default function SignUpPage() {
-  return <div>SignUpPage</div>;
+  return (
+    <div>
+      <h1>Sign up to Firasa</h1>
+      <div className="mt-10 flex flex-col gap-4 max-w-md mx-auto">
+        <Button variant="outline-variant" size="md">
+          <Google />
+          Continue with Google
+        </Button>
+        <Button variant="outline-variant" size="md">
+          <Apple />
+          Continue with Apple
+        </Button>
+        <Button variant="outline-variant" size="md">
+          <FacebookFilled />
+          Continue with Facebook
+        </Button>
+        <div
+          className="relative flex items-center text-sm text-gray-500
+            before:content-[''] before:flex-1 before:border-t before:border-error-container
+            after:content-[''] after:flex-1 after:border-t after:border-error-container"
+        >
+          <span className="px-3">OR</span>
+        </div>
+        <div className="group relative">
+          <label
+            className="-translate-y-1/2 absolute start-1 top-0 z-10 block bg-background px-2  text-xs group-has-disabled:opacity-50 body-small-primary text-on-surface-variant"
+            // htmlFor={id}
+          >
+            Full name *
+          </label>
+          <Input
+            className="p-4 placeholder:body-large-primary placeholder:text-on-surface-variant"
+            placeholder="Your name"
+          />
+        </div>
+        <div className="group relative">
+          <label
+            className="-translate-y-1/2 absolute start-1 top-0 z-10 block bg-background px-2  text-xs group-has-disabled:opacity-50 body-small-primary text-on-surface-variant"
+            // htmlFor={id}
+          >
+            Email *
+          </label>
+          <Input
+            className="p-4 placeholder:body-large-primary placeholder:text-on-surface-variant"
+            placeholder="mail@domain.com"
+            type="email"
+          />
+        </div>
+        <div className="group relative">
+          <label
+            className="-translate-y-1/2 absolute start-1 top-0 z-10 block bg-background px-2  text-xs group-has-disabled:opacity-50 body-small-primary text-on-surface-variant"
+            // htmlFor={id}
+          >
+            Password *
+          </label>
+          <InputGroup>
+            <InputGroupInput
+              id="inline-end-input"
+              className="p-4 placeholder:body-large-primary placeholder:text-on-surface-variant"
+              type="password"
+              placeholder="Enter password"
+            />
+            <InputGroupAddon align="inline-end">
+              <EyeOffIcon />
+            </InputGroupAddon>
+          </InputGroup>
+        </div>
+        <div className="group relative">
+          <label
+            className="-translate-y-1/2 absolute start-1 top-0 z-10 block bg-background px-2  text-xs group-has-disabled:opacity-50 body-small-primary text-on-surface-variant"
+            // htmlFor={id}
+          >
+            Confirm Password *
+          </label>
+          <InputGroup>
+            <InputGroupInput
+              id="inline-end-input"
+              className="p-4 placeholder:body-large-primary placeholder:text-on-surface-variant"
+              type="password"
+              placeholder="Confirm password"
+            />
+            <InputGroupAddon align="inline-end">
+              <EyeOffIcon />
+            </InputGroupAddon>
+          </InputGroup>
+        </div>
+        <Button>Sign Up</Button>
+        <div className="flex-center gap-3 body-large-primary text-outline">
+          <span>Already have an account? Sign in</span>
+        </div>
+      </div>
+    </div>
+  );
 }
