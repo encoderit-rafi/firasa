50493ac3a9b97a8274e8b0023f6a118cf1b00ba6
encoderit-rafi <rafi@encoderit.net>
2026-02-20 12:15:40 +0600

    table

--- src/components/blocks/pricing.tsx
+++ src/components/blocks/pricing.tsx
@@ -3,20 +3,18 @@ import {
   TableBody,
   TableCaption,
   TableCell,
+  TableDescription,
   TableFooter,
   TableHead,
   TableHeader,
+  TableHeading,
+  TablePrice,
   TableRow,
 } from "@/components/ui/table";
 const IsChecked = (value: boolean) => {
   const bg = value ? "bg-gradient" : "bg-error-container";
   return (
-    <div
-      className={cn(
-        bg,
-        "flex h-6 w-6 items-center justify-center rounded-full",
-      )}
-    >
+    <div className={cn(bg, "flex-center mx-auto size-6 rounded-full p-1")}>
       {value ? (
         <Check className="text-white" />
       ) : (
@@ -25,6 +23,108 @@ const IsChecked = (value: boolean) => {
     </div>
   );
 };
+const prices = [
+  {
+    title: "5-second video analysis",
+    plan: {
+      free: IsChecked(true),
+      pro: {
+        monthly: IsChecked(true),
+        annual: IsChecked(true),
+      },
+    },
+  },
+  {
+    title: "Big 5 personality scores",
+    plan: {
+      free: IsChecked(true),
+      pro: {
+        monthly: IsChecked(true),
+        annual: IsChecked(true),
+      },
+    },
+  },
+  {
+    title: "Automatic video deletion",
+    plan: {
+      free: IsChecked(true),
+      pro: {
+        monthly: IsChecked(true),
+        annual: IsChecked(true),
+      },
+    },
+  },
+  {
+    title: "Deep trait insights",
+    plan: {
+      free: IsChecked(false),
+      pro: {
+        monthly: IsChecked(true),
+        annual: IsChecked(true),
+      },
+    },
+  },
+  {
+    title: "Scientific reasoning",
+    plan: {
+      free: IsChecked(false),
+      pro: {
+        monthly: IsChecked(true),
+        annual: IsChecked(true),
+      },
+    },
+  },
+  {
+    title: "Your personality story",
+    plan: {
+      free: IsChecked(false),
+      pro: {
+        monthly: IsChecked(true),
+        annual: IsChecked(true),
+      },
+    },
+  },
+  {
+    title: "Growth & improvement plan",
+    plan: {
+      free: IsChecked(false),
+      pro: {
+        monthly: IsChecked(true),
+        annual: IsChecked(true),
+      },
+    },
+  },
+  {
+    title: "Compatibility profile",
+    plan: {
+      free: IsChecked(false),
+      pro: {
+        monthly: IsChecked(true),
+        annual: IsChecked(true),
+      },
+    },
+  },
+  {
+    title: "Famous personalities like you",
+    plan: {
+      free: IsChecked(false),
+      pro: {
+        monthly: IsChecked(false),
+        annual: IsChecked(true),
+      },
+    },
+  },
+  {
+    title: "Shareable social cards (no watermark)",
+    plan: {
+      free: IsChecked(false),
+      pro: {
+        monthly: IsChecked(false),
+        annual: IsChecked(true),
+      },
+    },
+  },
+];
 const invoices = [
   {
     totalAmount: IsChecked(true),
@@ -61,7 +161,38 @@ import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
 // import { Check } from "lucide-react";
 import { Check, Close } from "@/assets/icons";
+function FreeHeading() {
+  return (
+    <>
+      <TableHeading>Free</TableHeading>
+      <TableDescription>
+        No sign-up.
+        <br />
+        No credit card.
+      </TableDescription>
 
+      <TablePrice variant="muted">$0</TablePrice>
+    </>
+  );
+}
+function ProHeading() {
+  return (
+    <>
+      <TableHeading>Pro</TableHeading>
+      <TableDescription>
+        Made for sharing.
+        <br />
+        Built for self-growth.
+      </TableDescription>
+
+      <TablePrice variant="default">$79</TablePrice>
+      <TabsList>
+        <TabsTrigger value="monthly">Monthly</TabsTrigger>
+        <TabsTrigger value="annual">Annual (Save 10%)</TabsTrigger>
+      </TabsList>
+    </>
+  );
+}
 export default function Pricing() {
   return (
     <section className="section">
@@ -71,72 +202,100 @@ export default function Pricing() {
         Get your Big 5 score for free — unlock deeper insights when you’re
         ready.
       </p>
-      <div className="px-4">
-        <div className="container-sm mx-auto mt-8 space-y-6 lg:mt-16">
-          <Tabs defaultValue="overview">
-            <Table className="max-md:hidden">
+
+      <div className="container-sm mx-auto mt-8 space-y-6 px-4 lg:mt-16">
+        <Tabs defaultValue="overview">
+          <Table className="max-md:hidden">
+            <TableHeader>
+              <TableRow>
+                <TableHead className=""></TableHead>
+                <TableHead className="py-4 pt-8! text-center lg:text-end">
+                  <FreeHeading />
+                </TableHead>
+                <TableHead className="rounded-t-3xl bg-red-50 py-4 pt-8! text-center">
+                  <ProHeading />
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {prices.map((price, index) => (
+                <TableRow key={index} className="">
+                  <TableCell>{price.title}</TableCell>
+                  <TableCell>{price.plan.free}</TableCell>
+                  <TableCell className="bg-red-50 text-center">
+                    <TabsContent value="monthly">
+                      {price.plan.pro.monthly}
+                    </TabsContent>
+                    <TabsContent value="annual">
+                      {price.plan.pro.annual}
+                    </TabsContent>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter className="border-none bg-transparent">
+              <TableRow>
+                <TableCell className=""></TableCell>
+                <TableCell className="pb-8 text-center">
+                  <Button variant="outline">Get free score</Button>
+                </TableCell>
+                <TableCell
+                  className={"rounded-b-3xl bg-red-50 pb-8 text-center"}
+                >
+                  <Button variant="black">Unlock pro report</Button>
+                </TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+          <div className="space-y-6 md:hidden">
+            <div className="border-error-container overflow-hidden rounded-3xl border py-8">
+              <Table className="">
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="py-4 text-center" colSpan={2}>
+                      <FreeHeading />
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {prices.map((price, index) => (
+                    <TableRow key={index} className="">
+                      <TableCell>{price.title}</TableCell>
+                      <TableCell className="">{price.plan.free}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+                <TableFooter className="border-none bg-transparent">
+                  <TableRow>
+                    <TableCell className="text-center" colSpan={2}>
+                      <Button variant="outline">Get free score</Button>
+                    </TableCell>
+                  </TableRow>
+                </TableFooter>
+              </Table>
+            </div>
+
+            <Table className="">
               <TableHeader>
                 <TableRow>
-                  <TableHead className=""></TableHead>
-                  <TableHead className="py-4 pt-8! text-center lg:text-end">
-                    <p className="headline-small-emphasized">Free</p>
-                    <p className="title-small-emphasized text-outline">
-                      No sign-up.
-                      <br />
-                      No credit card.
-                    </p>
-                    <p className="display-large-emphasized text-error-container mt-8 text-center">
-                      $0
-                    </p>
-                  </TableHead>
-                  <TableHead className="rounded-t-3xl bg-red-50 py-4 pt-8! text-center">
-                    <p className="headline-small-emphasized">Pro</p>
-                    <p className="text-outline title-small-emphasized">
-                      Made for sharing.
-                      <br />
-                      Built for self-growth.
-                    </p>
-                    <p className="bg-gradient display-large-emphasized bg-clip-text text-transparent">
-                      $79
-                    </p>
-                    <TabsList>
-                      <TabsTrigger value="overview">Overview</TabsTrigger>
-                      <TabsTrigger value="analytics">Analytics</TabsTrigger>
-                    </TabsList>
+                  <TableHead
+                    className="rounded-t-3xl bg-red-50 py-4 pt-8! text-center"
+                    colSpan={2}
+                  >
+                    <ProHeading />
                   </TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {invoices.map((invoice, index) => (
+                {prices.map((price, index) => (
                   <TableRow key={index} className="">
-                    <TableCell>{invoice.paymentMethod}</TableCell>
-                    <TableCell className="">
-                      <div
-                        className={cn(
-                          "bg-gradient mx-auto size-6 rounded-full",
-                        )}
-                      >
-                        <Check className="text-white" />
-                      </div>
-                    </TableCell>
+                    <TableCell className="bg-red-50">{price.title}</TableCell>
                     <TableCell className="bg-red-50 text-center">
-                      <TabsContent value="overview">
-                        <div
-                          className={cn(
-                            "bg-gradient mx-auto size-6 rounded-full",
-                          )}
-                        >
-                          <Check className="text-white" />
-                        </div>
+                      <TabsContent value="monthly">
+                        {price.plan.pro.monthly}
                       </TabsContent>
-                      <TabsContent value="analytics">
-                        <div
-                          className={cn(
-                            "bg-gradient mx-auto size-6 rounded-full",
-                          )}
-                        >
-                          <Check className="text-white" />
-                        </div>
+                      <TabsContent value="annual">
+                        {price.plan.pro.annual}
                       </TabsContent>
                     </TableCell>
                   </TableRow>
@@ -144,128 +303,17 @@ export default function Pricing() {
               </TableBody>
               <TableFooter className="border-none bg-transparent">
                 <TableRow>
-                  <TableCell className=""></TableCell>
-                  <TableCell className="pb-8 text-center">
-                    <Button variant="outline">Get free score</Button>
-                  </TableCell>
                   <TableCell
                     className={"rounded-b-3xl bg-red-50 pb-8 text-center"}
+                    colSpan={2}
                   >
                     <Button variant="black">Unlock pro report</Button>
                   </TableCell>
                 </TableRow>
               </TableFooter>
             </Table>
-            <div className="space-y-6 md:hidden">
-              <div className="border-error-container overflow-hidden rounded-3xl border py-8">
-                <Table className="">
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead className="py-4 text-center" colSpan={2}>
-                        <p className="headline-small-emphasized">Free</p>
-                        <p className="title-small-emphasized text-outline">
-                          No sign-up.
-                          <br />
-                          No credit card.
-                        </p>
-                        <p className="display-large-emphasized text-error-container mt-8 text-center">
-                          $0
-                        </p>
-                      </TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    {invoices.map((invoice, index) => (
-                      <TableRow key={index} className="">
-                        <TableCell>{invoice.paymentMethod}</TableCell>
-                        <TableCell className="">
-                          <div
-                            className={cn(
-                              "bg-gradient ml-auto size-6 rounded-full lg:mx-auto",
-                            )}
-                          >
-                            <Check className="text-white" />
-                          </div>
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                  <TableFooter className="border-none bg-transparent">
-                    <TableRow>
-                      <TableCell className="text-center" colSpan={2}>
-                        <Button variant="outline">Get free score</Button>
-                      </TableCell>
-                    </TableRow>
-                  </TableFooter>
-                </Table>
-              </div>
-
-              <Table className="">
-                <TableHeader>
-                  <TableRow>
-                    <TableHead
-                      className="rounded-t-3xl bg-red-50 py-4 pt-8! text-center"
-                      colSpan={2}
-                    >
-                      <p className="headline-small-emphasized">Pro</p>
-                      <p className="text-outline title-small-emphasized">
-                        Made for sharing.
-                        <br />
-                        Built for self-growth.
-                      </p>
-                      <p className="bg-gradient display-large-emphasized bg-clip-text text-transparent">
-                        $79
-                      </p>
-                      <TabsList>
-                        <TabsTrigger value="overview">Overview</TabsTrigger>
-                        <TabsTrigger value="analytics">Analytics</TabsTrigger>
-                      </TabsList>
-                    </TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {invoices.map((invoice, index) => (
-                    <TableRow key={index} className="">
-                      <TableCell className="bg-red-50">
-                        {invoice.paymentMethod}
-                      </TableCell>
-                      <TableCell className="bg-red-50 text-center">
-                        <TabsContent value="overview">
-                          <div
-                            className={cn(
-                              "bg-gradient ml-auto size-6 rounded-full lg:mx-auto",
-                            )}
-                          >
-                            <Check className="text-white" />
-                          </div>
-                        </TabsContent>
-                        <TabsContent value="analytics">
-                          <div
-                            className={cn(
-                              "bg-gradient ml-auto size-6 rounded-full lg:mx-auto",
-                            )}
-                          >
-                            <Check className="text-white" />
-                          </div>
-                        </TabsContent>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-                <TableFooter className="border-none bg-transparent">
-                  <TableRow>
-                    <TableCell
-                      className={"rounded-b-3xl bg-red-50 pb-8 text-center"}
-                      colSpan={2}
-                    >
-                      <Button variant="black">Unlock pro report</Button>
-                    </TableCell>
-                  </TableRow>
-                </TableFooter>
-              </Table>
-            </div>
-          </Tabs>
-        </div>
+          </div>
+        </Tabs>
       </div>
     </section>
   );
